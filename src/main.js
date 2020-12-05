032c62ff2ab329aec327bcf6dff643a4d1149d00
alexNI <alekseynuzdin@gmail.com>
2020-12-05 14:10:23 +0700

    fix vue config

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ Vue.use(Vuelidate)
 
 Vue.config.productionTip = false;
 Vue.config.devtools = true;
-Vue.config.publicPath = '/portfolio/Fom_reg/';
+
 
 new Vue({
   render: function (h) { return h(App) },
